8b8f98fce5f366fb8fd40b428beb88cf9fbaa8c6
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2018-02-14 08:32:23 +0100

    Move hardcoded user name to component

--- src/app/layout/components/header/header.component.ts
+++ src/app/layout/components/header/header.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
-import { AuthService } from '../../../services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+import {TranslateService} from '@ngx-translate/core';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
     selector: 'app-header',
@@ -10,12 +10,11 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class HeaderComponent implements OnInit {
     pushRightClass: string = 'push-right';
+    private userName: string = 'username';
 
-    constructor(
-        private translate: TranslateService,
-        public router: Router,
-        private authService: AuthService
-    ) {
+    constructor(private translate: TranslateService,
+                public router: Router,
+                private authService: AuthService) {
 
         this.translate.addLangs(['en', 'fr', 'ur', 'es', 'it', 'fa', 'de']);
         this.translate.setDefaultLang('en');
@@ -33,7 +32,8 @@ export class HeaderComponent implements OnInit {
         });
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+    }
 
     isToggled(): boolean {
         const dom: Element = document.querySelector('body');
