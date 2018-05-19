5f66212f7ab9d2602fd3a5a7cf25045931a41cfa
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2018-05-19 14:33:04 +0200

    Add links for strategy descriptions

--- src/app/layout/automation/automation.component.ts
+++ src/app/layout/automation/automation.component.ts
@@ -1,54 +1,66 @@
-import { Component, OnInit } from '@angular/core';
-import { StrategiesExecutionsService } from '../../services/automation/strategies-executions.service';
-import { Observable } from 'rxjs';
-import { ToastService } from '../../services/toast.service';
-import { Exchange } from '../../models';
-import { ExchangesService } from '../../services/automation/exchanges.service';
-import { StrategyExecutionResponseDto } from '../../models/strategy';
+import {Component, OnInit} from '@angular/core';
+import {StrategiesExecutionsService} from '../../services/automation/strategies-executions.service';
+import {Observable} from 'rxjs';
+import {ToastService} from '../../services/toast.service';
+import {Client, Exchange} from '../../models';
+import {ExchangesService} from '../../services/automation/exchanges.service';
+import {StrategyExecutionResponseDto} from '../../models/strategy';
+import {ClientsService} from '../../services/api';
 
 @Component({
-  selector: 'app-automation',
-  templateUrl: './automation.component.html',
-  styleUrls: ['./automation.component.scss']
+    selector: 'app-automation',
+    templateUrl: './automation.component.html',
+    styleUrls: ['./automation.component.scss']
 })
 export class AutomationComponent implements OnInit {
 
-  exchanges: Exchange[];
+    exchanges: Exchange[] = [];
+    clients: Client[] = [];
+    private strategiesExecutions: StrategyExecutionResponseDto[];
 
-  private strategiesExecutions: StrategyExecutionResponseDto[];
+    constructor(
+        private toastService: ToastService,
+        private exchangesService: ExchangesService,
+        private strategiesExecutionsService: StrategiesExecutionsService,
+        private clientsService: ClientsService
+    ) {
+    }
 
-  constructor(
-    private toastService: ToastService,
-    private exchangesService: ExchangesService,
-    private strategiesExecutionsService: StrategiesExecutionsService
-  ) { }
+    ngOnInit() {
+        this.loadData();
+    }
 
-  ngOnInit() {
-    this.loadData();
-  }
+    loadData() {
+        Observable.forkJoin(
+            this.exchangesService.getExchanges(),
+            this.strategiesExecutionsService.getStrategiesExecutions(),
+            this.clientsService.getClients()
+        ).subscribe(([exchanges, strategiesExecutions, clients]) => {
+            this.exchanges = exchanges;
+            this.strategiesExecutions = strategiesExecutions;
+            this.clients = clients;
+            console.log(this.strategiesExecutions);
+        }, error => {
+            this.clients = [];
+            this.exchanges = [];
+            this.strategiesExecutions = [];
+            this.toastService.danger('Sorry, something went wrong.');
+        });
+    }
 
-  loadData() {
-    Observable.forkJoin(
-      this.exchangesService.getExchanges(),
-      this.strategiesExecutionsService.getStrategiesExecutions()
-    ).subscribe(([exchanges, strategiesExecutions]) => {
-      this.exchanges = exchanges;
-      this.strategiesExecutions = strategiesExecutions;
-      console.log(this.strategiesExecutions);
-    }, error => {
-      this.toastService.danger('Sorry, something went wrong.');
-    });
-  }
+    getStrategiesExecutionsByExchangeName(exchangeName): StrategyExecutionResponseDto[] {
+        return this.strategiesExecutions.filter(strategyExecution => {
+            return strategyExecution.exchangeName === exchangeName;
+        });
+    }
 
-  getStrategiesExecutionsByExchangeName(exchangeName) {
-    return this.strategiesExecutions.filter(strategyExecution => {
-      return strategyExecution.exchangeName == exchangeName;
-    });
-  }
+    getClientNameById(clientId: string): string {
+        return this.clients.find(client => client.id === clientId).name;
+    }
 
-  deleteStrategyExecution(strategyExecution) {
-    this.strategiesExecutionsService.deleteStrategyExecution(strategyExecution.id).subscribe(() => {
-      this.loadData();
-    });
-  }
+    deleteStrategyExecution(strategyExecution) {
+        this.strategiesExecutionsService.deleteStrategyExecution(strategyExecution.id).subscribe(() => {
+            this.loadData();
+        });
+    }
 }
