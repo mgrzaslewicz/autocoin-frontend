70430c14879f3bf82aadf8dc0515bb6608dfcf02
Mikołaj Grząślewicz <mikolaj.grzaslewicz@gmail.com>
2019-05-07 20:41:32 +0200

    Add minSellPrice to SellNow strategy and maxBuyPrice to BuyNow strategy

--- src/app/layout/trading-automation/add-strategy-execution/buy-now-parameters/buy-now-parameters.component.ts
+++ src/app/layout/trading-automation/add-strategy-execution/buy-now-parameters/buy-now-parameters.component.ts
@@ -8,7 +8,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 export class BuyNowParametersComponent implements OnInit {
 
     @Input()
-    strategySpecificParameters = {};
+    strategySpecificParameters = {
+        maxBuyPrice: 0.0
+    };
 
     @Output('specificParametersChanged')
     specificParametersChangedEmitter = new EventEmitter;
@@ -22,6 +24,28 @@ export class BuyNowParametersComponent implements OnInit {
 
     emitInput() {
         this.specificParametersChangedEmitter.emit(this.strategySpecificParameters);
+    }
+
+    private precision(a) {
+        if (!isFinite(a)) {
+            return 0;
+        }
+        let e = 1, p = 0;
+        while (Math.round(a * e) / e !== a) {
+            e *= 10;
+            p++;
+        }
+        return p;
+    }
+
+    onMaxBuyPrice(control) {
+        if (control.value) {
+            const value = Math.min(Math.max(control.value, 0.00000001), 9999999999);
+            const precision = this.precision(value);
+            control.value = Number(value).toFixed(precision);
+            this.strategySpecificParameters.maxBuyPrice = Number(control.value); // no idea why control.value has string type at this point so create number
+            this.emitInput();
+        }
     }
 
 }
