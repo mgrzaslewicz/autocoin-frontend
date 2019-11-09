da1b624829eac5f910eb65e7d8c8c46d73519138
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2019-11-09 14:29:19 +0100

    Extract arbitrage monitor profit tiles to separate component and improve view

--- src/app/layout/arbitrage-monitor/arbitrage-monitor.module.ts
+++ src/app/layout/arbitrage-monitor/arbitrage-monitor.module.ts
@@ -5,16 +5,19 @@ import {PageHeaderModule} from '../../shared';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ArbitrageMonitorRoutingModule} from './arbitrage-monitor-routing.module';
 import {ArbitrageMonitorComponent} from './arbitrage-monitor.component';
+import {TwoLegArbitrageProfitTileComponent} from './two-leg-arbitrage-monitor-profit-tile/two-leg-arbitrage-profit-tile.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    ArbitrageMonitorRoutingModule,
-    PageHeaderModule,
-    NgbModule,
-  ],
-  declarations: [
-    ArbitrageMonitorComponent
-  ]
+    imports: [
+        CommonModule,
+        ArbitrageMonitorRoutingModule,
+        PageHeaderModule,
+        NgbModule
+    ],
+    declarations: [
+        ArbitrageMonitorComponent,
+        TwoLegArbitrageProfitTileComponent
+    ]
 })
-export class ArbitrageMonitorModule { }
+export class ArbitrageMonitorModule {
+}
--- src/app/layout/arbitrage-monitor/two-leg-arbitrage-monitor-profit-tile/two-leg-arbitrage-profit-tile.component.ts
+++ src/app/layout/arbitrage-monitor/two-leg-arbitrage-monitor-profit-tile/two-leg-arbitrage-profit-tile.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {TwoLegArbitrageProfit} from '../../../services/arbitrage-monitor.service';
 import {animate, state, style, transition, trigger} from '@angular/animations';
-import {ExchangeHealth} from '../health.component';
 
 @Component({
-    selector: 'app-health-panel',
-    templateUrl: './health-panel.component.html',
-    styleUrls: ['./health-panel.component.scss'],
+    selector: 'app-two-leg-arbitrage-profit-tile',
+    templateUrl: './two-leg-arbitrage-profit-tile.component.html',
+    styleUrls: ['./two-leg-arbitrage-profit-tile.component.scss'],
     animations: [
         trigger('myAnimation', [
             state('in', style({
@@ -21,8 +21,8 @@ import {ExchangeHealth} from '../health.component';
         ])
     ]
 })
-export class HealthPanelComponent implements OnInit {
-    @Input() exchangeHealth: ExchangeHealth;
+export class TwoLegArbitrageProfitTileComponent {
+    @Input() twoLegArbitrageProfitOpportunity: TwoLegArbitrageProfit;
 
     showDetails = false;
     slide = 'out';
@@ -30,11 +30,9 @@ export class HealthPanelComponent implements OnInit {
     constructor() {
     }
 
-    ngOnInit() {
-    }
-
     toggleDetailsVisibility() {
         this.showDetails = !this.showDetails;
         this.slide = this.slide === 'in' ? 'out' : 'in';
     }
+
 }
