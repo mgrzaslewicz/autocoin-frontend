92e975ec648df743bd651b043822e2757bc5848d
Mikolaj Grzaslewicz <mgrzaslewicz@atlassian.com>
2022-06-13 16:24:05 +0200

    Add table for exchanges health

--- src/app/layout/health/health.component.ts
+++ src/app/layout/health/health.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {ExchangeHealthDto, HealthService} from '../../services/health.service';
 import {ToastService} from '../../services/toast.service';
 import {AuthService} from '../../services/auth.service';
+import { isNumeric } from 'rxjs/internal/util/isNumeric';
 
 @Component({
     selector: 'app-health',
@@ -9,7 +10,9 @@ import {AuthService} from '../../services/auth.service';
     styleUrls: ['health.component.scss']
 })
 export class HealthComponent implements OnInit {
-    exchangesHealth: Map<String, Array<ExchangeHealthDto>> = new Map<String, Array<ExchangeHealthDto>>();
+    exchangesHealthGroupedByCategory: Map<String, Array<ExchangeHealthDto>> = new Map<String, Array<ExchangeHealthDto>>();
+    exchangesHealth: Array<ExchangeHealthDto>;
+    capabilities: string[];
 
     constructor(private healthService: HealthService,
                 private toastService: ToastService,
@@ -23,37 +26,39 @@ export class HealthComponent implements OnInit {
     }
 
     healthCategories(): String[] {
-        return Array.from(this.exchangesHealth.keys());
+        return Array.from(this.exchangesHealthGroupedByCategory.keys());
     }
 
     private getExchangesHealth() {
         this.healthService.getExchangesHealth().subscribe(
             healthDtoList => {
+                this.exchangesHealth = healthDtoList;
+                this.capabilities = this.extractCapabilitiesFromExchangesHealth();
                 console.log(healthDtoList);
-                this.exchangesHealth.set('Unhealthy', healthDtoList.filter(it =>
+                this.exchangesHealthGroupedByCategory.set('Unhealthy', healthDtoList.filter(it =>
                     !it.canGetPublicMarketData
                 ).sort((a, b) => {
                     return a.exchangeName.localeCompare(b.exchangeName);
                 }));
-                this.exchangesHealth.set('Ok for public data', healthDtoList.filter(it => {
+                this.exchangesHealthGroupedByCategory.set('Ok for public data', healthDtoList.filter(it => {
                     return it.canGetPublicMarketData;
                 }).sort((a, b) => {
                     return a.exchangeName.localeCompare(b.exchangeName);
                 }));
-                this.exchangesHealth.set('Ok for detailed arbitrage monitoring', healthDtoList.filter(it => {
+                this.exchangesHealthGroupedByCategory.set('Ok for detailed arbitrage monitoring', healthDtoList.filter(it => {
                     return it.okForDetailedArbitrage;
                 }).sort((a, b) => {
                     return a.exchangeName.localeCompare(b.exchangeName);
                 }));
-                this.exchangesHealth.set('Ok for trading', healthDtoList.filter(it => {
+                this.exchangesHealthGroupedByCategory.set('Ok for trading', healthDtoList.filter(it => {
                     return it.healthy;
                 }).sort((a, b) => {
                     return a.exchangeName.localeCompare(b.exchangeName);
                 }));
-                this.exchangesHealth.set('All exchanges', healthDtoList.sort((a, b) => {
+                this.exchangesHealthGroupedByCategory.set('All exchanges', healthDtoList.sort((a, b) => {
                     return a.exchangeName.localeCompare(b.exchangeName);
                 }));
-                console.log(this.exchangesHealth);
+                console.log(this.exchangesHealthGroupedByCategory);
             },
             error => {
                 this.toastService.warning('Could not get exchanges health');
@@ -62,8 +67,15 @@ export class HealthComponent implements OnInit {
     }
 
     hasNoExchangesInCategory(exchangeHealthCategory: String) {
-        const exchanges = this.exchangesHealth.get(exchangeHealthCategory);
+        const exchanges = this.exchangesHealthGroupedByCategory.get(exchangeHealthCategory);
         return exchanges === undefined || exchanges === null || exchanges.length === 0;
     }
 
+    private extractCapabilitiesFromExchangesHealth(): string[] {
+        return this.exchangesHealth[0].capabilities.map(it => {return it.name});
+    }
+
+    isNumeric(value: string): boolean {
+        return isNumeric(value);
+    }
 }
