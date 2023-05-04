695f3138d90b13679fcc369bd7cb96982b277463
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-05-04 21:52:27 +0200

    Fail fast docker entry point when wrong config provided

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,4 +1,8 @@
+set -e
 CONFIG=${CONFIG:-default}
 CONFIG_FILE="/config/config.${CONFIG}.json"
-ln -s "${CONFIG_FILE}" /usr/share/nginx/html/assets/config.json
+
+DO_NOT_FAIL_SILENTLY_ALTHOUGH_DOCS_DOES_NOT_MENTION_IT="v"
+
+ln -s${DO_NOT_FAIL_SILENTLY_ALTHOUGH_DOCS_DOES_NOT_MENTION_IT} "${CONFIG_FILE}" /usr/share/nginx/html/assets/config.json
 nginx -g "daemon off;"
