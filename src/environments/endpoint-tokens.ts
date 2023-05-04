c1af47f8e6361e12cc004aaea150e8bce7fd5823
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-05-04 18:27:24 +0200

    Remove unused ExchangeMetadataEndpointUrlToken

--- src/environments/endpoint-tokens.ts
+++ src/environments/endpoint-tokens.ts
@@ -4,7 +4,6 @@ export const AuthServiceUrlToken = new InjectionToken('AuthServiceUrl');
 export const BalanceMonitorApiBaseUrlToken = new InjectionToken('BalanceMonitorEndpointUrlToken');
 export const ExchangeKeysCapabilityEndpointUrlToken = new InjectionToken('ExchangeKeysCapabilityEndpointUrlToken');
 export const PricesEndpointUrlToken = new InjectionToken('PricesEndpointUrlToken');
-export const ExchangeMetadataEndpointUrlToken = new InjectionToken('ExchangeMetadataEndpointUrlToken');
 export const StrategiesEndpointUrlToken = new InjectionToken('StrategiesEndpointUrlToken');
 export const HealthEndpointUrlToken = new InjectionToken('HealthEndpointUrlToken');
 export const ArbitrageMonitorEndpointUrlToken = new InjectionToken('ArbitrageMonitorEndpointUrlToken');
