f0be55436b117cdcb480f619f462f53061fe11fd
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-05-04 17:49:18 +0200

    Rename base services url vars to reflect their meaning

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 import {defaultEnvironment} from './environment.default';
 
-const authServiceHost = 'https://users-apiv2.autocoin-trader.com';
+const authServiceUrl = 'https://users-apiv2.autocoin-trader.com';
 const exchangeMediatorServiceUrl = 'https://orders-api.autocoin-trader.com';
 const strategyExecutorServiceUrl = 'https://strategies-api.autocoin-trader.com';
-const arbitrageMonitorHost = 'https://arbitrage-monitor.autocoin-trader.com';
-const balanceMonitorHost = 'https://balance-monitor.autocoin-trader.com';
+const arbitrageMonitorUrl = 'https://arbitrage-monitor.autocoin-trader.com';
+const balanceMonitorUrl = 'https://balance-monitor.autocoin-trader.com';
 
 async function fetchConfig() {
     const request = new XMLHttpRequest();
@@ -26,20 +26,20 @@ async function fetchConfig() {
 export const environment = {
     ...defaultEnvironment,
     production: true,
-    oauthEndpointUrl: `${authServiceHost}/oauth/token`,
-    exchangeUsersApiUrl: authServiceHost,
-    twoFactorAuthenticationEndpointUrl: `${authServiceHost}/user-accounts/2fa`,
-    changePasswordEndpointUrl: `${authServiceHost}/user-accounts/password`,
-    requestEmailWithResetPasswordTokenEndpointUrl: `${authServiceHost}/user-accounts/password/reset-with-token/step-1-send-email-with-token`,
-    authServiceEndpointUrl: authServiceHost,
-    changePasswordWithResetPasswordTokenEndpointUrl: `${authServiceHost}/user-accounts/password/reset-with-token/step-2-change-password`,
+    oauthEndpointUrl: `${authServiceUrl}/oauth/token`,
+    exchangeUsersApiUrl: authServiceUrl,
+    twoFactorAuthenticationEndpointUrl: `${authServiceUrl}/user-accounts/2fa`,
+    changePasswordEndpointUrl: `${authServiceUrl}/user-accounts/password`,
+    requestEmailWithResetPasswordTokenEndpointUrl: `${authServiceUrl}/user-accounts/password/reset-with-token/step-1-send-email-with-token`,
+    authServiceEndpointUrl: authServiceUrl,
+    changePasswordWithResetPasswordTokenEndpointUrl: `${authServiceUrl}/user-accounts/password/reset-with-token/step-2-change-password`,
     ordersEndpointUrl: exchangeMediatorServiceUrl,
     exchangeKeysCapabilityEndpointUrl: exchangeMediatorServiceUrl,
     pricesEndpointUrl: `${exchangeMediatorServiceUrl}/prices`,
     strategiesEndpointUrl: strategyExecutorServiceUrl,
     healthEndpointUrl: `${exchangeMediatorServiceUrl}/health`,
-    signupEndpointUrl: `${authServiceHost}/user-accounts`,
-    arbitrageMonitorEndpointUrl: arbitrageMonitorHost,
-    balanceMonitorApiBaseUrl: balanceMonitorHost,
+    signupEndpointUrl: `${authServiceUrl}/user-accounts`,
+    arbitrageMonitorEndpointUrl: arbitrageMonitorUrl,
+    balanceMonitorApiBaseUrl: balanceMonitorUrl,
     ...fetchConfig(),
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,28 +4,28 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 import {defaultEnvironment} from './environment.default';
 
-const authServiceHost = 'http://localhost:9002';
+const authServiceUrl = 'http://localhost:9002';
 const exchangeMediatorServiceUrl = 'http://localhost:9001';
 const strategyExecutorServiceUrl = 'http://localhost:9021';
-const arbitrageMonitorHost = 'http://localhost:10021';
-const balanceMonitorHost = 'http://localhost:10022'
+const arbitrageMonitorUrl = 'http://localhost:10021';
+const balanceMonitorUrl = 'http://localhost:10022'
 
 export const environment = {
     ...defaultEnvironment,
     production: false,
-    oauthEndpointUrl: `${authServiceHost}/oauth/token`,
-    exchangeUsersApiUrl: authServiceHost,
-    twoFactorAuthenticationEndpointUrl: `${authServiceHost}/user-accounts/2fa`,
-    changePasswordEndpointUrl: `${authServiceHost}/user-accounts/password`,
-    requestEmailWithResetPasswordTokenEndpointUrl: `${authServiceHost}/user-accounts/password/reset-with-token/step-1-send-email-with-token`,
-    authServiceEndpointUrl: authServiceHost,
-    changePasswordWithResetPasswordTokenEndpointUrl: `${authServiceHost}/user-accounts/password/reset-with-token/step-2-change-password`,
+    oauthEndpointUrl: `${authServiceUrl}/oauth/token`,
+    exchangeUsersApiUrl: authServiceUrl,
+    twoFactorAuthenticationEndpointUrl: `${authServiceUrl}/user-accounts/2fa`,
+    changePasswordEndpointUrl: `${authServiceUrl}/user-accounts/password`,
+    requestEmailWithResetPasswordTokenEndpointUrl: `${authServiceUrl}/user-accounts/password/reset-with-token/step-1-send-email-with-token`,
+    authServiceEndpointUrl: authServiceUrl,
+    changePasswordWithResetPasswordTokenEndpointUrl: `${authServiceUrl}/user-accounts/password/reset-with-token/step-2-change-password`,
     ordersEndpointUrl: exchangeMediatorServiceUrl,
     exchangeKeysCapabilityEndpointUrl: exchangeMediatorServiceUrl,
     pricesEndpointUrl: `${exchangeMediatorServiceUrl}/prices`,
     strategiesEndpointUrl: strategyExecutorServiceUrl,
     healthEndpointUrl: `${exchangeMediatorServiceUrl}/health`,
-    signupEndpointUrl: `${authServiceHost}/user-accounts`,
-    arbitrageMonitorEndpointUrl: arbitrageMonitorHost,
-    balanceMonitorApiBaseUrl: balanceMonitorHost
+    signupEndpointUrl: `${authServiceUrl}/user-accounts`,
+    arbitrageMonitorEndpointUrl: arbitrageMonitorUrl,
+    balanceMonitorApiBaseUrl: balanceMonitorUrl
 };
