45e15351910c6b60df15dd5fb86bf83c6934d8a1
Mikolaj Grzaslewicz <mikolaj.grzaslewicz@gmail.com>
2023-05-07 21:38:34 +0200

    Fix default (dev) config

--- src/environments/environment.default.ts
+++ src/environments/environment.default.ts
@@ -1,5 +1,11 @@
 import * as localhostConfig from '../config/config.localhost.json';
 
+interface JsonModule {
+    default: any;
+}
+
+const jsonLocalhostConfig = localhostConfig as unknown as JsonModule;
+
 export const defaultEnvironment = {
-    ...localhostConfig
+    ...jsonLocalhostConfig.default,
 };
